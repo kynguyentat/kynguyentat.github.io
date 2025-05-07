f60ec7a198bac2767867a5a5b244e578de525423
kynguyentat <ky.nt.2546@aptechlearning.edu.vn>
2025-05-07 18:23:41 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>Thông tin sinh viên</title>
+</head>
+<body>
+    <h2>Thông tin sinh viên</h2>
+    <?php
+        $hoTen = "Nguyễn Văn A";
+        $mssv = "PH01410";
+        
+        echo "Họ và tên: " . $hoTen . "<br>";
+        echo "MSSV: " . $mssv;
+    ?>
+</body>
+</html>
